1914d479f433d74c903a6e4d00f22187d7924eb3
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-03 11:28:48 +0100

    first stable version

--- main.py
+++ main.py
@@ -1,94 +1,88 @@
 from decouple import config
+
 import discord
 from discord.ext import commands
 import json
 import requests
+from time import sleep as sl
 
-from test_data import data
-lb_data = data
+def sleep(seconds):
+    print(f"Sleeping for {seconds} seconds")
+    sl(seconds)
 
 session_cookie=config('session_cookie')
+cookie = {'session': session_cookie}
 url=config('url')
 discord_token=config('discord_token')
 discord_guild=int(config('discord_guild'))
-discord_channel1=int(config('discord_channel1'))
-discord_channel2=int(config('discord_channel2'))
-discord_channel3=int(config('discord_channel3'))
+discord_channels = []
+discord_channels.append(int(config('discord_channel1')))
+discord_channels.append(int(config('discord_channel2')))
+discord_channels.append(int(config('discord_channel3')))
 
-# lb_data = {}
-cookie = {'session': session_cookie}
 
-def save_to_file(filename, variable, name):
-    file1 = open(filename, "w", encoding="utf-8")
-    file1.write(f"{name} = ")  # + str(variable).encode('utf8'))
-    file1.write(str(variable))
-    file1.close()
-    print(f"Saved as {filename}")
+
+async def get_sorted_leaderboard():
+
+    data = await fetch_leaderboard()
+    sorted_list = sorted(
+        ((value['local_score'], value['name']) for key, value in data['members'].items()),
+        reverse=True
+    )
+    print(sorted_list)
+    sorted_names = [name for score, name in sorted_list]
+    sorted_scores = [score for score, name in sorted_list]
+    print(sorted_names)
+    print(sorted_scores)
+    leaderboard = []
+    for i in range(0,3):
+        leaderboard.append(f"{sorted_scores[i]}⭐ {sorted_names[i]}")
+    print(leaderboard)
+    return leaderboard
 
 
 intents = discord.Intents.default()
-# intents.message_content = True
 client = discord.Client(intents=intents)
-# client=commands.Bot(intents=intents, command_prefix="!")
+
+
+async def change_vc(channel, new_name):
+    guild = client.get_guild(discord_guild)
+    if not guild:
+        print("Guild not found!")
+        return
+    voice_channel = guild.get_channel(channel)
+    if not voice_channel:
+        print("Voice channel not found!")
+        return
+    await voice_channel.edit(name=new_name)
+
+
 
 @ client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-#     guild = client.get_guild(discord_guild)
-#     if not guild:
-#         print("Guild not found!")
-#         return
-#     voice_channel = guild.get_channel(discord_channel1)
-#     if not voice_channel:
-#         print("Voice channel not found!")
-#         return
-#     await voice_channel.edit(name="Test")
-# @client.command(name='change_vc_name')
-# async def change_vc_name(ctx, *, new_name: str):
-#     guild = client.get_guild(discord_guild)
-#     if not guild:
-#         print("Guild not found!")
-#         return
-#     voice_channel = guild.get_channel(discord_channel1)
-#     if not voice_channel:
-#         print("Voice channel not found!")
-#         return
-#     await voice_channel.edit(name="Test")
+    i = 0
+    while True:
+        sorted_data = await get_sorted_leaderboard()
+        for i in range(0,3):
+            await change_vc(discord_channels[i], sorted_data[i])
+            print(f"Updated channel {i} to {sorted_data[i]}")
+            sleep(1)
+        sleep(60 * 15)
 
-
-@ client.event
-async def on_message(message):
-    # print(message.content)
-    # print(dir(message))
-    guild = client.get_guild(discord_guild)
-    print(guild)
-    if not guild:
-        print("Guild not found!")
-        return
-    voice_channel = guild.get_channel(discord_channel1)
-    if not voice_channel:
-        print("Voice channel not found!")
-        return
-    await voice_channel.edit(name="Test")
-    # if(message.)
-
-def fetch_leaderboard():
+async def fetch_leaderboard():
+    print("Fetching new leaderboard data")
     response = requests.get(
         url,
         timeout=60,
         cookies=cookie
         )
 
-
     if response.status_code == 200:
-        # Parse the JSON data into a dictionary
         lb_data = response.json()
-        # lb_data = data
-        # Now, `data` is a Python dictionary containing the JSON data
-        # print(data)
-        # save_to_file("test_data.py", lb_data, "data")
+        return lb_data
     else:
         print("Failed to fetch data: HTTP Status Code", response.status_code)
-
+    return None
 
 client.run(discord_token)
